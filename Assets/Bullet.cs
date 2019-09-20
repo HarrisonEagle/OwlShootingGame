524d1f424c310c4fc9a5792bc5fdb0b639aa0985
HarrisonKawagoe <hyzitbios@hotmail.com>
2019-09-20 17:02:11 +0900

    First Commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 vec = player.transform.position;
+        bullet.transform.position = vec;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 vec = bullet.transform.position;
+        vec.y += 0.2f;
+        bullet.transform.position = vec;
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject bullet;
+    public GameObject enemy;
+    
+
+    IEnumerator Shoot()
+    {
+        
+        while (true)
+        {
+            GameObject obj = Instantiate(bullet);
+            yield return new WaitForSeconds(0.1f);
+        }
+        // 何か処理
+
+
+    }
+
+    IEnumerator CreateEnemy()
+    {
+
+        while (true)
+        {
+
+            GameObject obj = Instantiate(enemy);
+            yield return new WaitForSeconds(0.1f);
+        }
+        // 何か処理
+
+
+    }
+
+    void OnTriggerEnter2D(Collider other)
+    {
+        string layerName = LayerMask.LayerToName(other.gameObject.layer);
+
+        if (layerName == "enemy")
+        {
+            Destroy(player);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Shoot());
+        StartCoroutine(CreateEnemy());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector2 vec = player.transform.position;
+
+            vec.y += 0.1f;
+
+            player.transform.position = vec;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector2 vec = player.transform.position;
+
+            vec.y -= 0.1f;
+
+            player.transform.position = vec;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector2 vec = player.transform.position;
+
+            vec.x -= 0.1f;
+
+            player.transform.position = vec;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector2 vec = player.transform.position;
+
+            vec.x += 0.1f;
+
+            player.transform.position = vec;
+        }
+    }
+}
